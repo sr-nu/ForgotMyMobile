e143e7878a536ae0725215c94a65f482a51f4466
Srikanth <srikanth.nutigattu@gmail.com>
2014-04-15 05:55:57 +0800

    pushing response to service

--- app/src/main/src/com/forgotMyMobile/BackgroundService.java
+++ app/src/main/src/com/forgotMyMobile/BackgroundService.java
@@ -1,4 +1,47 @@
 package com.forgotMyMobile;
 
+import android.app.PendingIntent;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.telephony.SmsManager;
+import android.util.Log;
+
 public class BackgroundService {
+    public void respond(Context context, String replyToAddress){
+        ContentResolver contentResolver = context.getContentResolver();
+        Cursor cursor = contentResolver.query( Uri.parse("content://sms/inbox"), null, null, null, null);
+
+        int indexBody = cursor.getColumnIndex("BODY");
+        int indexAddr = cursor.getColumnIndex("ADDRESS");
+
+        if ( indexBody < 0 || !cursor.moveToFirst() ) return;
+
+        String messages= "";
+
+        do
+        {
+            String str = "Sender: " + cursor.getString( indexAddr ) + "\n" + cursor.getString( indexBody );
+            messages += str;
+        }
+        while( cursor.moveToNext() );
+
+
+        sendSMS(replyToAddress,messages,context);
+
+    }
+
+    private void sendSMS(String phoneNumber, String message, Context context)
+    {
+    	
+    	Log.i("SendSMS","Sending"+message+" to : "+phoneNumber);
+        PendingIntent pi = PendingIntent.getService(context, 0,
+                new Intent("SMS_SENT"), 0);
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, pi, null);
+    }
+
+
 }
--- app/src/main/src/com/forgotMyMobile/SmsReceiver.java
+++ app/src/main/src/com/forgotMyMobile/SmsReceiver.java
@@ -25,9 +25,6 @@ public class SmsReceiver extends BroadcastReceiver{
     	Toast.makeText(context,"SMSReceiver on receive",Toast.LENGTH_LONG).show();
 
         Bundle extras = intent.getExtras();
-        String messages = "";
-
-        String replyToAddress = "";
 
         if ( extras != null )
         {
@@ -41,11 +38,13 @@ public class SmsReceiver extends BroadcastReceiver{
 
                 if (body.contains("1234")) {
                     Log.i("SMSReceiver", "sms received");
-                    replyToAddress = address;
-
-                    respond(context, replyToAddress);
-                    this.abortBroadcast();
-                    Toast.makeText(context, "Control Msg"+replyToAddress, Toast.LENGTH_SHORT).show();
+                  
+                    //make a call to service to respond to the SMS
+                    
+                    
+                   //                    respond(context, replyToAddress);
+                   // this.abortBroadcast();
+                    Toast.makeText(context, "Control Msg"+address, Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(context, "Normal Msg"+address, Toast.LENGTH_SHORT).show();
                 }
@@ -54,37 +53,6 @@ public class SmsReceiver extends BroadcastReceiver{
 
         }
 
-    }
-
-    public void respond(Context context, String replyToAddress){
-        ContentResolver contentResolver = context.getContentResolver();
-        Cursor cursor = contentResolver.query( Uri.parse("content://sms/inbox"), null, null, null, null);
-
-        int indexBody = cursor.getColumnIndex("BODY");
-        int indexAddr = cursor.getColumnIndex("ADDRESS");
-
-        if ( indexBody < 0 || !cursor.moveToFirst() ) return;
-
-        String messages= "";
-
-        do
-        {
-            String str = "Sender: " + cursor.getString( indexAddr ) + "\n" + cursor.getString( indexBody );
-            messages += str;
-        }
-        while( cursor.moveToNext() );
-
-
-        sendSMS(replyToAddress,messages,context);
-
-    }
-
-    private void sendSMS(String phoneNumber, String message, Context context)
-    {
-        PendingIntent pi = PendingIntent.getService(context, 0,
-                new Intent("SMS_SENT"), 0);
-        SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage(phoneNumber, null, message, pi, null);
     }
 
 
