5a4b0042b998c6f0aa7c5f0253bb2bc852f60bd9
Srikanth <srikanth.nutigattu@gmail.com>
2014-04-15 13:39:39 +0800

    added field to capture passcode

--- app/src/main/gen/com/forgotMyMobile/R.java
+++ app/src/main/gen/com/forgotMyMobile/R.java
@@ -17,11 +17,14 @@ public final class R {
         public static final int MainLayout=0x7f050000;
         public static final int SMSList=0x7f050002;
         public static final int UpdateList=0x7f050001;
+        public static final int passcode=0x7f050003;
+        public static final int setPasscode=0x7f050004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int set_passcode=0x7f040001;
     }
 }
